771c5f876df2d7ee230661e057389ae3c1f3af6f
shun91 <shun9167@gmail.com>
2022-06-01 23:01:46 +0900

    ✨ コンポーネントの自動インポートを試す

---
+++ components/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Nuxt3</h1>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+.container {
+  height: 100vh;
+  width: 100vw;
+  background: rgb(1, 42, 53);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+  font-family: helvetica, arial, sans-serif;
+  font-weight: 400;
+}
+</style>
