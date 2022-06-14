c6b02a5afb93bda2da9d5da1ed40866bfb865fa8
shun91 <shun9167@gmail.com>
2022-06-14 21:34:11 +0900

    ✨ use pages dir

--- app.vue
+++ app.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    <Head>
-      <Title>{{ title }}</Title>
-      <Style type="text/css" children="body { background-color: green; }"/>
-    </Head>
-    <Welcome />
-    {{ hello }}
+    <NuxtLayout><NuxtPage/></NuxtLayout>
   </div>
 </template>
-
-<script setup>
-const title = ref('Hello World')
-
-const hello = await $fetch('/api/hello/nuxt')
-
-const postTest = await $fetch('/api/test', { method: 'post'})
-
-$fetch('/api/submit', { method: 'post', body: { test: 123 } })
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Head>
+      <Title>{{ title }}</Title>
+      <Style type="text/css" children="body { background-color: green; }"/>
+    </Head>
+    <Welcome />
+    {{ hello }}
+  </div>
+</template>
+
+<script setup>
+const title = ref('Hello World')
+
+const hello = await $fetch('/api/hello/nuxt')
+
+const postTest = await $fetch('/api/test', { method: 'post'})
+
+$fetch('/api/submit', { method: 'post', body: { test: 123 } })
+</script>
