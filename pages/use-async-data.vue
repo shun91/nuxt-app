e3847ed1063de41dc3880c2c6b2b90c946eb5808
shun91 <shun9167@gmail.com>
2022-06-21 21:36:00 +0900

    ✨ useAsyncData example

---
+++ pages/use-async-data.vue
@@ -0,0 +1,10 @@
+<template>
+  Page visits: {{ data }}
+</template>
+
+<script setup>
+const { data } = await useAsyncData('count', async () => {
+  const c = await $fetch('/api/count')
+  return c.count
+})
+</script>
